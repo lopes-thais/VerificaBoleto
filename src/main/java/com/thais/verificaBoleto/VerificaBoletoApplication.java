b67900b6305547af798b01040b03f113a0f3e291
lopes-thais <lopesthais.emp@gmail.com>
2026-07-16 22:55:36 -0300

    feat: inicializa projeto Spring Boot

---
+++ src/main/java/com/thais/verificaBoleto/VerificaBoletoApplication.java
@@ -0,0 +1,13 @@
+package com.thais.verificaBoleto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerificaBoletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerificaBoletoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thais/verificaBoleto/VerificaBoletoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thais.verificaBoleto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerificaBoletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
